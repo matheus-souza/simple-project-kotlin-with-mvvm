ed498c6f156140979be757294ae8879a7bc424c8
Matheus H. de Souza <matheussouza@aiqfome.com>
2020-08-14 22:53:32 -0300

    added toolbar

--- app/src/main/java/com/valhalla/nybooks/ui/books/BooksActivity.kt
+++ app/src/main/java/com/valhalla/nybooks/ui/books/BooksActivity.kt
@@ -3,10 +3,14 @@ package com.valhalla.nybooks.ui.books
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.valhalla.nybooks.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbarMain.title = "Books"
+        setSupportActionBar(toolbarMain)
     }
 }
