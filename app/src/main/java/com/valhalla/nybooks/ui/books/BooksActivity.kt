e25c58c4a59dc65559a6102c0d126f27e4b62467
Matheus H. de Souza <matheussouza@aiqfome.com>
2020-08-14 22:32:33 -0300

    organizing project

--- app/src/main/java/com/valhalla/nybooks/ui/books/BooksActivity.kt
+++ app/src/main/java/com/valhalla/nybooks/ui/books/BooksActivity.kt
@@ -1,9 +1,10 @@
-package com.valhalla.nybooks
+package com.valhalla.nybooks.ui.books
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.valhalla.nybooks.R
 
-class MainActivity : AppCompatActivity() {
+class BooksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
