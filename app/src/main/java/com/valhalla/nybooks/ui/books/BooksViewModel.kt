bfa5367d63a070d3ebcf84f7020594cbf425f1d7
Matheus H. de Souza <matheussouza@aiqfome.com>
2020-08-16 17:26:49 -0300

    implemented request and item listing

--- app/src/main/java/com/valhalla/nybooks/ui/books/BooksViewModel.kt
+++ app/src/main/java/com/valhalla/nybooks/ui/books/BooksViewModel.kt
@@ -3,20 +3,43 @@ package com.valhalla.nybooks.ui.books
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.valhalla.nybooks.data.model.Book
+import com.valhalla.nybooks.data.module.ApiModule
+import com.valhalla.nybooks.data.response.BookBodyResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BooksViewModel : ViewModel() {
 
     val booksLiveData: MutableLiveData<List<Book>> = MutableLiveData()
 
     fun getBooks() {
-        booksLiveData.value = createFakeBooks()
-    }
+        ApiModule.service.getBooks().enqueue(object: Callback<BookBodyResponse> {
+            override fun onResponse(
+                call: Call<BookBodyResponse>,
+                response: Response<BookBodyResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val books: MutableList<Book> = mutableListOf()
 
-    fun createFakeBooks(): List<Book> {
-        return listOf(
-            Book("title", "autor"),
-            Book("title2", "autor2"),
-            Book("title3", "autor3")
-        )
+                    response.body()?.let {bookBodyResponse ->
+                        for (result in bookBodyResponse.resultResponse) {
+                            val book = Book(
+                                title = result.bookDetailResponses[0].title,
+                                author = result.bookDetailResponses[0].author
+                            )
+
+                            books.add(book)
+                        }
+                    }
+
+                    booksLiveData.value = books
+                }
+            }
+
+            override fun onFailure(call: Call<BookBodyResponse>, t: Throwable) {
+
+            }
+        })
     }
 }
